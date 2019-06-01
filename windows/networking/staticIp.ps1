6306cbf6b8e891148bd6c9ed349f14269ec96619
Shannon Kuehn <shannon.kuehn@gmail.com>
2019-05-31 22:23:50 -0500

    Update staticIp.ps1

--- windows/networking/staticIp.ps1
+++ windows/networking/staticIp.ps1
@@ -1,10 +1,10 @@
-Param{
+Param(
     [string] [Parameter(Mandatory = $true)] $staticIp,
     [string] [Parameter(Mandatory = $true)] $cidrClass,
     [string] [Parameter(Mandatory = $true)] $gateway,
     [string] [Parameter(Mandatory = $true)] $dns1,
     [string] [Parameter(Mandatory = $true)] $dns2
-}
+)
 
 $wmi = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true'"
 $wmi.EnableStatic($staticIp, $cidrClass)
